35c4b7a976db1e6107248c9348c8f8a1d1106364
alexbobrob <bobrov.3alex@gmail.com>
2023-06-23 13:36:24 -0400

    feat: setup endpoints for auth

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-//Set up mongoose connection
+require('dotenv').config();
+
+const authRoutes = require('./routes/authRoutes');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,6 +10,8 @@ const port = process.env.PORT || 5000
 //setting up middlewares
 app.use(cors())
 app.use(express.json());
+app.use('/auth', authRoutes);
+
 
 app.listen(port, () => {
   console.log(`Listening to port ${port}`)
